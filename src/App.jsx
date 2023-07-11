f70f46e616346768285a10f4893ff8fe73d7be5c
Daniela <danscarballo@gmail.com>
2023-07-11 16:00:12 -0300

    Seleccion de turnos

--- src/App.jsx
+++ src/App.jsx
@@ -5,31 +5,42 @@ const TURNS = {
   O: "o",
 };
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
-  const [board, setBoard] = useState(['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']);
-
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
 
   return (
     <main className="board">
-      
       <h1>Tic Tac Toe</h1>
 
       <section className="game">
-        {
-          board.map((_, index) => {
-            return (
-              <Square key={index} index={index}>
-                {board[index]}
-              </Square>
-            );
-          })
-        }
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
       </section>
 
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
     </main>
   );
 }
