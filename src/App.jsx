48071e12f05ee82753d08271e56ea93aef1ce90a
Daniela <danscarballo@gmail.com>
2023-07-11 15:29:55 -0300

    Setea a mano el estado del tablero

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import "./App.css";
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x']);
 
-  return <h1>Tic Tac Toe</h1>;
+
+  return (
+    <main className="board">
+      
+      <h1>Tic Tac Toe</h1>
+
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index}>
+                {board[index]}
+              </Square>
+            );
+          })
+        }
+      </section>
+
+    </main>
+  );
 }
 
 export default App;
